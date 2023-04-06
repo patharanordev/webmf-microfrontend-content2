119ce8de4b665fbb06a1c971bcfcb1f7d12b9708
Pathara Norasethasopon <pathara.no@bdms.co.th>
2023-04-06 18:03:41 +0700

    Expose content

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:8080/",
+    publicPath: "http://localhost:8582/",
   },
 
   resolve: {
@@ -12,7 +12,7 @@ module.exports = (_, argv) => ({
   },
 
   devServer: {
-    port: 8080,
+    port: 8582,
     historyApiFallback: true,
   },
 
@@ -44,7 +44,9 @@ module.exports = (_, argv) => ({
       name: "content2",
       filename: "remoteEntry.js",
       remotes: {},
-      exposes: {},
+      exposes: {
+        "./Area2": "./src/ContentArea2"
+      },
       shared: {
         ...deps,
         react: {
